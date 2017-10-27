0d7b5a65c2841f15a393b9b2ff676edaa092c1e6
Bogdan Dobrelya <bogdando@mail.ru>
2017-10-27 15:20:09 +0000

    Devbox WIP
    Fix mounted volumes for devbox
    Use detached devbox
    Few dns fixes
    
    Signed-off-by: Bogdan Dobrelya <bogdando@mail.ru>

--- devbox.sh
+++ devbox.sh
@@ -1,12 +1,10 @@
 #!/bin/bash                                                                                                                                                                       
 docker run \
     -u $(id -u $USER):$(id -g $USER) \
-    -it \
-    --rm \
+    -itd \
     --privileged \
     --cap-add=ALL \
     --net=host --uts=host --pid=host --ipc=host \
-    --device /dev/snd \
     -e GPG_AGENT_INFO \
     -e SSH_AUTH_SOCK \
     -e DISPLAY \
@@ -15,6 +13,8 @@ docker run \
     -e LANG=en_US.UTF-8 \
     -e WINEPREFIX=/home/devbox/wine \
     -v $(pwd)/lightdm.conf:/etc/lightdm/lightdm.conf:ro \
+    -v $(pwd)/resolv.conf:/etc/resolv.conf:ro \
+    -v /etc/NetworkManager/dnsmasq.d:/etc/NetworkManager/dnsmasq.d:ro \
     -v /etc/dbus-1:/etc/dbus-1 \
     -v /run:/run \
     -v /var/run/docker.sock:/var/run/docker.sock \
@@ -28,8 +28,6 @@ docker run \
     -v /var/lib/libvirt:/var/lib/libvirt \
     -v /etc/apt/:/etc/apt/ \
     -v /var/cache/apt:/var/cache/apt \
-    -v /opt/docker-data/wine/.cache:/home/devbox/.cache \
-    -v /opt/docker-data/wine:/home/devbox/wine \
     -v $(pwd)/examples:/templates:ro \
     -v /opt/pr:/opt/pr \
     -v /opt/Projects:/opt/Projects \
@@ -41,8 +39,14 @@ docker run \
     -v /opt/sublime_text_2:/opt/sublime_text_2 \
     -v /opt/vagrant:/opt/vagrant \
     -v /opt/zoom:/opt/zoom \
+    -v /usr/bin/p4merge:/usr/bin/p4merge:ro \
+    -v /usr/bin/p4merge.bin:/usr/bin/p4merge.bin:ro \
+    -v /usr/lib/p4v:/usr/lib/p4v:ro \
     -v /usr/local/sbin/git_edit.sh:/usr/local/sbin/git_edit.sh:ro \
     -v /usr/local/bin/virtualenvwrapper.sh:/usr/local/bin/virtualenvwrapper.sh:ro \
     -v $HOME:/home/$USER \
     --name devbox \
     bogdando/devbox:v0.1 sudo vglrun /templates/devbox.template
+    
+#-v /opt/docker-data/wine/.cache:/home/devbox/.cache \
+#-v /opt/docker-data/wine:/home/devbox/wine \
