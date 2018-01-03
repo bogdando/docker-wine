b9c1df144b4cd16c4fd25c8e138a8d570e43f218
Bogdan Dobrelya <bogdando@mail.ru>
2018-01-03 10:55:47 +0100

    Fix h3 template
    Signed-off-by: Bogdan Dobrelya <bogdando@mail.ru>

--- h3_.sh
+++ h3_.sh
@@ -1,18 +1,29 @@
 #!/bin/bash
+# Firstly, install on a host: nvidia-docker and docker-ce, and nvidia drivers.
 # Build the image 1st with:
-# docker build -f Dockerfile_alt -t bogando/nvidia-virtualgl:2.5.2 .
+# docker build -f Dockerfile_alt -t bogdando/nvidia-virtualgl:2.5.2 .
+# These are specific to the host, like nvidia-304 driver installed
+nver=340.104
+DOCKER_VISUAL_NVIDIA="-v /usr/lib/x86_64-linux-gnu/libXau.so.6.0.0:/usr/lib/x86_64-linux-gnu/libXau.so.6.0.0:ro -v /usr/lib/x86_64-linux-gnu/libXdmcp.so.6:/usr/lib/x86_64-linux-gnu/libXdmcp.so.6:ro -v /usr/lib/x86_64-linux-gnu/libXext.so.6:/usr/lib/x86_64-linux-gnu/libXext.so.6:ro -v /usr/lib/x86_64-linux-gnu/libXdmcp.so.6.0.0:/usr/lib/x86_64-linux-gnu/libXdmcp.so.6.0.0:ro -v /usr/lib/x86_64-linux-gnu/libX11.so.6.3.0:/usr/lib/x86_64-linux-gnu/libX11.so.6.3.0:ro -v /usr/lib/x86_64-linux-gnu/libxcb.so.1:/usr/lib/x86_64-linux-gnu/libxcb.so.1:ro -v /usr/lib/nvidia-${nver%.*}:/usr/lib/nvidia-${nver%.*}:ro -v /usr/lib32/nvidia-${nver%.*}:/usr/lib32/nvidia-${nver%.*}:ro"
+
 xhost +si:localuser:$(whoami) >/dev/null
 xhost +local:root
 docker run \
+    --runtime=nvidia \
+    -e LIBGL_DEBUG="verbose glxgears" \
+    --privileged \
     --rm \
     -ti \
     -u root \
     -e DISPLAY \
     -e WINEPREFIX=/home/h3/wine \
+    -e LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:/usr/lib/nvidia-${nver%.*}:/usr/lib32/nvidia-${nver%.*}" \
     -v /usr/lib/x86_64-linux-gnu/libXv.so.1:/usr/lib/x86_64-linux-gnu/libXv.so.1:ro \
     -v /tmp/.X11-unix:/tmp/.X11-unix:ro \
     -v /opt/docker-data/wine/.cache:/home/h3/.cache \
     -v /opt/docker-data/wine:/home/h3/wine \
+    -v /lib/modules:/lib/modules:ro \
+    ${DOCKER_VISUAL_NVIDIA} \
     -v $(pwd)/examples:/templates:ro \
     -v /opt/pr:/opt/pr \
     bogdando/nvidia-virtualgl:2.5.2 /bin/bash -c \
